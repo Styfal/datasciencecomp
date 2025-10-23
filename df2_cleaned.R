29d82be7c0291656fc8a4cce33ea0caf52222e08
Anish Bhattacharjee <157962833+Styfal@users.noreply.github.com>
2025-10-23 16:39:21 +0900

    Update df2_cleaned.R
    Follow the exact same row conditions

--- df2_cleaned.R
+++ df2_cleaned.R
@@ -1,92 +1,61 @@
+df2 <- read.csv("consumption_lower.csv")
+View(df2)
 
-########################################
-df2 <- read.csv("sn02.csv")
-
-df2 <- df2 %>% 
-  slice(-(1:9))
-
-df2 <- df2 %>% 
-  select(-(X:X.9))
-
-df2 <- df2 %>% 
-  select(-(X.24:X.29))
-
-df2 <- df2 %>% 
-  select(-(X.22))
+df2 <- df2 %>%
+  select(-(X:X.11))
 
 df2 <- df2 %>%
   select(-(X.13:X.15))
 
 df2 <- df2 %>%
-  select(-(X.11))
+  select(-(X.22))
 
 df2 <- df2 %>%
-  select(-(X.10))
+  select(-(X.24:X.29))
 
 df2 <- df2 %>% 
-  slice(-(1:4))
-
-colnames(df2) <- c("items", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "items(English)")
-
-
-df2 <- df2 %>% 
-  slice(-(13))
-
-df2 <- df2 %>% 
-  slice(-(14:28))
-
-df2 <- df2 %>% 
-  slice(-(14:251))
-
-
-df2 <- df2 %>% 
-  slice(-(15:31))
-
-
-df2 <- df2 %>% 
-  slice(-(17:19))
-
-
-
-df2 <- df2 %>% 
-  slice(-(18:21))
-
-
-df2 <- df2 %>% 
-  slice(-(19:71))
-
-
-
-df2 <- df2 %>% 
-  slice(-(19:119))
-
-
-
-df2 <- df2 %>% 
-  slice(-(22:36))
-
-df2 <- df2 %>% 
-  slice(-(23:26))
-
-df2 <- df2 %>% 
-  slice(-(25:36))
-
-df2 <- df2 %>% 
-  slice(-(37:39))
-
-df2 <- df2 %>% 
-  slice(-(41:199))
-
-df2 <- df2 %>% 
-  slice(-(42:74))
-
-df2 <- df2 %>% 
-  slice(-(45:56))
+  slice(-(1:13))
 
 View(df2)
 
+colnames(df2) <- c("items", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "items(English)")
+
+df2 <- df2 %>% 
+  slice(-(3:10))
+
+df2 <- df2 %>% 
+  slice(-(5:259))
 
 
+df2 <- df2 %>% 
+  slice(-(6:23))
 
 
+df2 <- df2 %>% 
+  slice(-(7:8))
+
+
+df2 <- df2 %>% 
+  slice(-(8:143))
+
+df2 <- df2 %>% 
+  slice(-(9:34))
+
+df2 <- df2 %>% 
+  slice(-(9:17))
+
+df2 <- df2 %>% 
+  slice(-(10:33))
+
+df2 <- df2 %>% 
+  slice(-(24:192))
+
+df2 <- df2 %>% 
+  slice(-(9))
+
+
+df2 <- df2 %>% 
+  slice(-(24:54))
+
+View(df2)
 
